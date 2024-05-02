cc551ac050c9b033db350adaba9bf72bb5597018
terryherbert <46780257+terryherbert@users.noreply.github.com>
2024-05-02 22:30:05 +0100

    Spilt Screen support
    Update UpstreamPacketHandler.java
    
    Fix merge
    
    Update GeyserSession.java
    
    Re-add missing import
    
    Update LoginEncryptionUtils.java
    
    Tidy up code
    
    Refractor and tidy
    
    Fix rebase issue
    
    Code tidying, some minor bug fixes and some new features that need porting
    
    Remove duplicate lines
    
    Refractor
    
    Tidy
    
    Remove unnecessary code
    
    fix rebase issue

--- core/src/main/java/org/geysermc/geyser/network/CodecProcessor.java
+++ core/src/main/java/org/geysermc/geyser/network/CodecProcessor.java
@@ -295,7 +295,6 @@ class CodecProcessor {
             .updateSerializer(CraftingEventPacket.class, ILLEGAL_SERIALIZER)
             // Illegal unusued serverbound packets that relate to unused features
             .updateSerializer(ClientCacheBlobStatusPacket.class, ILLEGAL_SERIALIZER)
-            .updateSerializer(SubClientLoginPacket.class, ILLEGAL_SERIALIZER)
             .updateSerializer(SubChunkRequestPacket.class, ILLEGAL_SERIALIZER)
             .updateSerializer(GameTestRequestPacket.class, ILLEGAL_SERIALIZER)
             // Illegal bidirectional packets
