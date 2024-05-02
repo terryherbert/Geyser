5d23010e88a511f7848c18ad9b91481766b2516c
THerbertBJSS <terry.herbert@bjss.com>
2024-05-02 22:30:05 +0100

    Spilt Screen support

--- core/src/main/java/org/geysermc/geyser/util/LoginEncryptionUtils.java
+++ core/src/main/java/org/geysermc/geyser/util/LoginEncryptionUtils.java
@@ -28,7 +28,8 @@ package org.geysermc.geyser.util;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import net.raphimc.minecraftauth.step.msa.StepMsaDeviceCode;
+import com.github.steveice10.mc.auth.service.MsaAuthenticationService;
+
 import org.cloudburstmc.protocol.bedrock.packet.LoginPacket;
 import org.cloudburstmc.protocol.bedrock.packet.ServerToClientHandshakePacket;
 import org.cloudburstmc.protocol.bedrock.packet.SubClientLoginPacket;
